13485e22c80f227f875ed727ee8b88a2a8d43743
Alexis <alexis6132@gmail.com>
2024-08-13 10:16:07 -0500

    larger card and touch fix

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('birthdayCard').addEventListener('click', function() {
+document.getElementById('birthdayCard').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default behavior
     this.classList.toggle('open');
 });
 
-document.getElementById('birthdayCard').addEventListener('touchstart', function() {
+document.getElementById('birthdayCard').addEventListener('touchstart', function(event) {
+    event.preventDefault(); // Prevent default behavior
     this.classList.toggle('open');
 });
