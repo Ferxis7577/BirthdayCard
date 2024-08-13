9be9de2b7d5defb60d049499f7ee99e78e100186
Alexis <alexis6132@gmail.com>
2024-08-13 10:02:16 -0500

    touch enable

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('birthdayCard').addEventListener('click', function() {
     this.classList.toggle('open');
 });
+
+document.getElementById('birthdayCard').addEventListener('touchstart', function() {
+    this.classList.toggle('open');
+});
